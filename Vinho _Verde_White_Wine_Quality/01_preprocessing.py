9e9d56ea410cada68889824344ee7bed1652dd5c
KatonaMihaly <katona0607@gmail.com>
2025-01-06 21:18:28 +0100

    Added: check for correlated feature, selecting highly correlated features, check for multicollinearity, trained the model and calculated scores.

--- Vinho _Verde_White_Wine_Quality/01_preprocessing.py
+++ Vinho _Verde_White_Wine_Quality/01_preprocessing.py
@@ -1,15 +1,19 @@
 """This algorithm deals with the preprocessing of the investigated data."""
 
 # Import external libraries to utilise for the preprocessing------------------------------------------------------------
-
+import numpy as np
 import pandas as pd
 import os
-
-# Get the absolute path of the folder-----------------------------------------------------------------------------------
 import seaborn as sns
 from matplotlib import pyplot as plt
 from imblearn.over_sampling import RandomOverSampler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
 
+oversample = True if input('Press "y" for oversampling and "x" for no oversampling.') == 'y' else False
+
+# Get the absolute path of the folder-----------------------------------------------------------------------------------
 folder_path = os.path.dirname(os.path.abspath(__file__))
 
 # Open the .csv file; the with function ensure that the .csv is closed after use----------------------------------------
@@ -36,39 +40,104 @@ sns.countplot(data=df_initial, x='quality')
 df_initial['quality'].value_counts()
 plt.show()
 
-# Oversampling underrepresented target variables------------------------------------------------------------------------
-# Class imbalance in a dataset
-majority_class = df_initial[df_initial['quality'] == 6]
-
-minority_class3 = df_initial[df_initial['quality'] == 3]
-minority_class4 = df_initial[df_initial['quality'] == 4]
-minority_class5 = df_initial[df_initial['quality'] == 6]
-minority_class7 = df_initial[df_initial['quality'] == 7]
-minority_class8 = df_initial[df_initial['quality'] == 8]
-minority_class9 = df_initial[df_initial['quality'] == 9]
-
-oversampler = RandomOverSampler()
-
-oversampled_minority3 = minority_class3.sample(n=len(majority_class), random_state=42, replace=True)
-df_oversampled = pd.concat([oversampled_minority3, majority_class])
-oversampled_minority4 = minority_class4.sample(n=len(majority_class), random_state=42, replace=True)
-df_oversampled = pd.concat([oversampled_minority4, df_oversampled])
-oversampled_minority5 = minority_class5.sample(n=len(majority_class), random_state=42, replace=True)
-df_oversampled = pd.concat([oversampled_minority5, df_oversampled])
-oversampled_minority7 = minority_class7.sample(n=len(majority_class), random_state=42, replace=True)
-df_oversampled = pd.concat([oversampled_minority7, df_oversampled])
-oversampled_minority8 = minority_class8.sample(n=len(majority_class), random_state=42, replace=True)
-df_oversampled = pd.concat([oversampled_minority8, df_oversampled])
-oversampled_minority9 = minority_class8.sample(n=len(majority_class), random_state=42, replace=True)
-df_oversampled = pd.concat([oversampled_minority9, df_oversampled])
-
-print(df_oversampled['quality'])
-
-# Checking for quality distribution-------------------------------------------------------------------------------------
-plt.figure(figsize=(6, 4), dpi=100)
-sns.countplot(data=df_oversampled, x='quality')
-df_oversampled['quality'].value_counts()
+# Checking for correlation matrix---------------------------------------------------------------------------------------
+plt.figure(figsize=(12, 8), dpi=100)
+sns.heatmap(df_initial.corr(), vmin=-1, vmax=1, cmap='icefire', annot=True)
 plt.show()
+
+# Selecting highly correlated features to the target variable-----------------------------------------------------------
+relevant_features = df_initial.corr()['quality'][abs(df_initial.corr()['quality']) > 0.1]
+print(relevant_features)
+print()
+
+# Keep relevant features----------------------------------------------------------------------------------------------
+columns_to_keep = relevant_features.index.tolist()
+df_relevant = df_initial[columns_to_keep]
+
+# Check for multicollinearity between the features----------------------------------------------------------------------
+plt.figure(figsize=(12, 8), dpi=100)
+sns.heatmap(df_relevant.corr(), vmin=-1, vmax=1, cmap='icefire', annot=True)
+plt.show()
+
+# Delete density as it has high correlation with alcohol----------------------------------------------------------------
+del df_relevant['density']
+
+df_temp = df_initial.copy(deep=True)
+
+# Oversampling underrepresented target variables------------------------------------------------------------------------
+if oversample:
+    majority_class = df_initial[df_initial['quality'] == 6]
+
+    minority_class3 = df_initial[df_initial['quality'] == 3]
+    minority_class4 = df_initial[df_initial['quality'] == 4]
+    minority_class5 = df_initial[df_initial['quality'] == 5]
+    minority_class7 = df_initial[df_initial['quality'] == 7]
+    minority_class8 = df_initial[df_initial['quality'] == 8]
+    minority_class9 = df_initial[df_initial['quality'] == 9]
+
+    oversampler = RandomOverSampler()
+
+    oversampled_minority3 = minority_class3.sample(n=len(majority_class), random_state=42, replace=True)
+    df_oversampled = pd.concat([oversampled_minority3, majority_class])
+    oversampled_minority4 = minority_class4.sample(n=len(majority_class), random_state=42, replace=True)
+    df_oversampled = pd.concat([oversampled_minority4, df_oversampled])
+    oversampled_minority5 = minority_class5.sample(n=len(majority_class), random_state=42, replace=True)
+    df_oversampled = pd.concat([oversampled_minority5, df_oversampled])
+    oversampled_minority7 = minority_class7.sample(n=len(majority_class), random_state=42, replace=True)
+    df_oversampled = pd.concat([oversampled_minority7, df_oversampled])
+    oversampled_minority8 = minority_class8.sample(n=len(majority_class), random_state=42, replace=True)
+    df_oversampled = pd.concat([oversampled_minority8, df_oversampled])
+    oversampled_minority9 = minority_class9.sample(n=len(majority_class), random_state=42, replace=True)
+    df_oversampled = pd.concat([oversampled_minority9, df_oversampled])
+
+    df_temp = df_oversampled.copy(deep=True)
+
+    # Checking for quality distribution---------------------------------------------------------------------------------
+    plt.figure(figsize=(6, 4), dpi=100)
+    sns.countplot(data=df_oversampled, x='quality')
+    df_oversampled['quality'].value_counts()
+    plt.show()
+
+# Splitting data into training and testing set--------------------------------------------------------------------------
+X = df_temp.drop('quality', axis=1)
+Y = df_temp['quality']
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20, random_state=42)
+
+# Training the model ---------------------------------------------------------------------------------------------------
+model = RandomForestClassifier()
+model.fit(X_train, Y_train)
+Y_prediction = model.predict(X_test)
+
+# Calculate Mean Absolute Error (MAE)-----------------------------------------------------------------------------------
+mae = mean_absolute_error(Y_test, Y_prediction)
+
+# Calculate Mean Squared Error (MSE)------------------------------------------------------------------------------------
+mse = mean_squared_error(Y_test, Y_prediction)
+
+# Calculate Root Mean Squared Error (RMSE)------------------------------------------------------------------------------
+rmse = np.sqrt(mse)
+
+# Calculate R-squared (R^2) score---------------------------------------------------------------------------------------
+r2 = r2_score(Y_test, Y_prediction)
+
+# Print the scores------------------------------------------------------------------------------------------------------
+print(f'MAE: {mae}, MSE: {mse}, RMSE: {rmse}, R^2: {r2}')
+print()
+
+# Plot the scores-------------------------------------------------------------------------------------------------------
+metrics = ['MAE', 'MSE', 'RMSE', 'R^2']
+values = [mae, mse, rmse, r2]
+plt.clf()
+plt.bar(metrics, values)
+plt.yticks(fontsize=20)
+plt.xticks(fontsize=20)
+plt.xlabel('Scores', fontsize=20)
+plt.ylabel('Values', fontsize=20)
+plt.gca().yaxis.grid(True, linestyle='-', alpha=0.5)
+plt.show()
+
+
+
 
 
 # Create a deepcopy of the initial dataframe to extend it with 'good' if quality > 5 else 'bad'
