48dab2df853a9525ef6afb64db7d7aed6bebdd21
KatonaMihaly <katona0607@gmail.com>
2025-01-20 13:42:52 +0100

    Decision Tree labeling algorithm.

--- Vinho _Verde_White_Wine_Quality/algorithms/02_RFC_2labels.py
+++ Vinho _Verde_White_Wine_Quality/algorithms/02_RFC_2labels.py
@@ -8,8 +8,12 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 from imblearn.over_sampling import RandomOverSampler
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, confusion_matrix, precision_score, \
+    accuracy_score, recall_score
+from sklearn.model_selection import train_test_split, cross_val_score
+
+# To compare oversampling to none---------------------------------------------------------------------------------------
+oversample = True if input('Press "y" for oversampling and "x" for no oversampling.') == 'y' else False
 
 # Get the absolute path of the folder-----------------------------------------------------------------------------------
 folder_path = os.path.dirname(os.path.abspath(__file__))
@@ -21,7 +25,7 @@ with open(folder_path + '/initial_data/white_wine_quality.csv', 'r') as f:
 # Create a deepcopy of the initial dataframe to modify it with 'good' if quality > 5 else 'bad'
 df_two_label = df_initial.copy(deep=True)
 
-df_two_label['quality'] = ['good' if df_initial['quality'][i] > 5 else 'bad' for i in range(len(df_initial))]
+df_two_label['quality'] = [1 if df_initial['quality'][i] > 5 else 0 for i in range(len(df_initial))]
 
 # Checking for missing values-------------------------------------------------------------------------------------------
 print(out := 'There are missing values!' if (df_initial.isna().sum() != 0).any() else 'There are no missing values.')
@@ -41,112 +45,230 @@ df_two_label.reset_index(drop=True, inplace=True)
 plt.figure(figsize=(6, 4), dpi=100)
 sns.countplot(data=df_two_label, x='quality')
 df_two_label['quality'].value_counts()
-plt.savefig('01_RFC_2labels_figure_01', dpi=100)
+plt.savefig('02_RFC_2labels_figure_01', dpi=100)
 plt.show()
 
-# # Checking for correlation matrix---------------------------------------------------------------------------------------
-# plt.figure(figsize=(12, 8), dpi=100)
-# sns.heatmap(df_initial.corr(), vmin=-1, vmax=1, cmap='icefire', annot=True)
-# plt.savefig('01_RFC_7labels_figure_02', dpi=100)
-#
-#
-# # Selecting highly correlated features to the target variable-----------------------------------------------------------
-# relevant_features = df_initial.corr()['quality'][abs(df_initial.corr()['quality']) > 0.1]
-# print(relevant_features)
-# print()
-#
-# # Keep relevant features----------------------------------------------------------------------------------------------
-# columns_to_keep = relevant_features.index.tolist()
-# df_relevant = df_initial[columns_to_keep]
-#
-# # Check for multicollinearity between the features----------------------------------------------------------------------
-# plt.figure(figsize=(12, 8), dpi=100)
-# sns.heatmap(df_relevant.corr(), vmin=-1, vmax=1, cmap='icefire', annot=True)
-# plt.savefig('01_RFC_7labels_figure_03', dpi=100)
-# plt.show()
-#
-# # Delete density as it has high correlation with alcohol----------------------------------------------------------------
-# if not both:
-#     if feature_selection:
-#         del df_relevant['density']
-#     else:
-#         del df_relevant['alcohol']
-# else:
-#     del df_relevant['density']
-#     del df_relevant['alcohol']
-# df_temp = df_initial.copy(deep=True)
-#
-# # Oversampling underrepresented target variables------------------------------------------------------------------------
-# if oversample:
-#     majority_class = df_initial[df_initial['quality'] == 6]
-#
-#     minority_class3 = df_initial[df_initial['quality'] == 3]
-#     minority_class4 = df_initial[df_initial['quality'] == 4]
-#     minority_class5 = df_initial[df_initial['quality'] == 5]
-#     minority_class7 = df_initial[df_initial['quality'] == 7]
-#     minority_class8 = df_initial[df_initial['quality'] == 8]
-#     minority_class9 = df_initial[df_initial['quality'] == 9]
-#
-#     oversampler = RandomOverSampler()
-#
-#     oversampled_minority3 = minority_class3.sample(n=len(majority_class), random_state=42, replace=True)
-#     df_oversampled = pd.concat([oversampled_minority3, majority_class])
-#     oversampled_minority4 = minority_class4.sample(n=len(majority_class), random_state=42, replace=True)
-#     df_oversampled = pd.concat([oversampled_minority4, df_oversampled])
-#     oversampled_minority5 = minority_class5.sample(n=len(majority_class), random_state=42, replace=True)
-#     df_oversampled = pd.concat([oversampled_minority5, df_oversampled])
-#     oversampled_minority7 = minority_class7.sample(n=len(majority_class), random_state=42, replace=True)
-#     df_oversampled = pd.concat([oversampled_minority7, df_oversampled])
-#     oversampled_minority8 = minority_class8.sample(n=len(majority_class), random_state=42, replace=True)
-#     df_oversampled = pd.concat([oversampled_minority8, df_oversampled])
-#     oversampled_minority9 = minority_class9.sample(n=len(majority_class), random_state=42, replace=True)
-#     df_oversampled = pd.concat([oversampled_minority9, df_oversampled])
-#
-#     df_temp = df_oversampled.copy(deep=True)
-#
-#     # Checking for quality distribution---------------------------------------------------------------------------------
-#     plt.figure(figsize=(6, 4), dpi=100)
-#     sns.countplot(data=df_oversampled, x='quality')
-#     df_oversampled['quality'].value_counts()
-#     plt.savefig(f'01_RFC_7labels_figure_{"both" if both else "wboth" if not both else "alcohol" if feature_selection else "density"}', dpi=100)
-#     plt.show()
-#
-# # Splitting data into training and testing set--------------------------------------------------------------------------
-# X = df_temp.drop('quality', axis=1)
-# Y = df_temp['quality']
-# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20, random_state=42)
-#
-# # Training the model ---------------------------------------------------------------------------------------------------
-# model = RandomForestClassifier()
-# model.fit(X_train, Y_train)
-# Y_prediction = model.predict(X_test)
-#
-# # Calculate Mean Absolute Error (MAE)-----------------------------------------------------------------------------------
-# mae = mean_absolute_error(Y_test, Y_prediction)
-#
-# # Calculate Mean Squared Error (MSE)------------------------------------------------------------------------------------
-# mse = mean_squared_error(Y_test, Y_prediction)
-#
-# # Calculate Root Mean Squared Error (RMSE)------------------------------------------------------------------------------
-# rmse = np.sqrt(mse)
-#
-# # Calculate R-squared (R^2) score---------------------------------------------------------------------------------------
-# r2 = r2_score(Y_test, Y_prediction)
-#
-# # Print the scores------------------------------------------------------------------------------------------------------
-# print(f'MAE: {mae}, MSE: {mse}, RMSE: {rmse}, R^2: {r2}')
-# print()
-#
-# # Plot the scores-------------------------------------------------------------------------------------------------------
-# metrics = ['MAE', 'MSE', 'RMSE', 'R^2']
-# values = [mae, mse, rmse, r2]
-# plt.clf()
-# plt.bar(metrics, values)
-# plt.yticks(fontsize=20)
-# plt.xticks(fontsize=20)
-# plt.xlabel('Scores', fontsize=20)
-# plt.ylabel('Values', fontsize=20)
-# plt.gca().yaxis.grid(True, linestyle='-', alpha=0.5)
-# plt.savefig(f'01_RFC_7labels_figure_{"both" if both else "wboth" if not both else "alcohol" if feature_selection else "density"}_'
-#             f'{"oversampled" if oversample else "notoversampled"}', dpi=100)
-# plt.show()
+# Checking for correlation matrix---------------------------------------------------------------------------------------
+plt.figure(figsize=(12, 8), dpi=100)
+sns.heatmap(df_two_label.corr(), vmin=-1, vmax=1, cmap='icefire', annot=True)
+plt.savefig('02_RFC_7labels_figure_02', dpi=100)
+plt.show()
+
+# Selecting highly correlated features to the target variable-----------------------------------------------------------
+relevant_features = df_two_label.corr()['quality'][abs(df_two_label.corr()['quality']) > 0.1]
+print(relevant_features)
+print()
+
+# Keep relevant features----------------------------------------------------------------------------------------------
+columns_to_keep = relevant_features.index.tolist()
+df_relevant = df_two_label[columns_to_keep]
+
+# Check for multicollinearity between the features----------------------------------------------------------------------
+plt.figure(figsize=(12, 8), dpi=100)
+sns.heatmap(df_relevant.corr(), vmin=-1, vmax=1, cmap='icefire', annot=True)
+plt.savefig('02_RFC_7labels_figure_03', dpi=100)
+plt.show()
+
+# Oversampling underrepresented target variables------------------------------------------------------------------------
+if oversample:
+    majority_class = df_relevant[df_relevant['quality'] == 1]
+
+    minority_class = df_relevant[df_relevant['quality'] == 0]
+
+    oversampler = RandomOverSampler()
+
+    oversampled_minority = minority_class.sample(n=len(majority_class), random_state=42, replace=True)
+    df_oversampled = pd.concat([oversampled_minority, majority_class])
+
+    df_temp = df_oversampled.copy(deep=True)
+
+    # Checking for quality distribution---------------------------------------------------------------------------------
+    plt.figure(figsize=(6, 4), dpi=100)
+    sns.countplot(data=df_oversampled, x='quality')
+    df_oversampled['quality'].value_counts()
+    plt.savefig(f'02_RFC_7labels_figure_04', dpi=100)
+    plt.show()
+
+    # Splitting data into training and testing set----------------------------------------------------------------------
+    X = df_temp.drop('quality', axis=1)
+    Y = df_temp['quality']
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20, random_state=42)
+
+    # Training the model------------------------------------------------------------------------------------------------
+    model = RandomForestClassifier()
+    model.fit(X_train, Y_train)
+    Y_prediction = model.predict(X_test)
+
+    # Create a confusion matrix-----------------------------------------------------------------------------------------
+    cm = confusion_matrix(Y_test, Y_prediction)
+    cm_df = pd.DataFrame(cm, index=['Actual Negative', 'Actual Positive'],
+                         columns=['Predicted Negative', 'Predicted Positive'])
+    plt.figure(figsize=(8, 6))
+    heatmap = sns.heatmap(cm_df, annot=True, fmt='d', cmap='Blues', annot_kws={"size": 18})
+    heatmap.set_xticklabels(heatmap.get_xticklabels(), fontsize=14)
+    heatmap.set_yticklabels(heatmap.get_yticklabels(), fontsize=14)
+
+    plt.title('Confusion Matrix')
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+    plt.savefig(f'02_RFC_7labels_figure_05', dpi=100)
+    plt.show()
+
+    precision = precision_score(Y_test, Y_prediction)
+    accuracy = accuracy_score(Y_test, Y_prediction)
+    recall = recall_score(Y_test, Y_prediction)
+    print("Precision:", precision)
+    print("Accuracy:", accuracy)
+    print("Recall (Sensitivity):", recall)
+
+    cv = []
+    precision1 = []
+    precision2 = []
+    accuracy1 = []
+    accuracy2 = []
+    sensitivity1 = []
+    sensitivity2 = []
+
+    for i in range(2, 21, 1):
+        scores = cross_val_score(model, X, Y, cv=i, scoring="precision", )
+        precision1.append(scores.mean())
+        precision2.append(scores.std())
+        scores = cross_val_score(model, X, Y, cv=i, scoring="accuracy")
+        accuracy1.append(scores.mean())
+        accuracy2.append(scores.std())
+        scores = cross_val_score(model, X, Y, cv=i, scoring="recall")
+        sensitivity1.append(scores.mean())
+        sensitivity2.append(scores.std())
+        cv.append(i)
+
+    data1 = pd.DataFrame(columns=['CV', 'Precision', 'Accuracy', 'Sensitivity'])
+    data1['CV'] = cv
+    data1['Precision'] = precision1
+    data1['Accuracy'] = accuracy1
+    data1['Sensitivity'] = sensitivity1
+
+    data2 = pd.DataFrame(columns=['CV', 'Precision', 'Accuracy', 'Sensitivity'])
+    data2['CV'] = cv
+    data2['Precision'] = precision2
+    data2['Accuracy'] = accuracy2
+    data2['Sensitivity'] = sensitivity2
+
+    # Plot each column as a scatter plot
+    plt.figure(figsize=(8, 6))
+    for column in ['Precision', 'Accuracy', 'Sensitivity']:
+        plt.scatter(data1['CV'], data1[column], label=column)
+
+    plt.xlabel('Index')
+    plt.ylabel('Values')
+    plt.title('MEAN')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f'02_RFC_7labels_figure_06', dpi=100)
+    plt.show()
+
+    # Plot each column as a scatter plot
+    plt.figure(figsize=(8, 6))
+    for column in ['Precision', 'Accuracy', 'Sensitivity']:
+        plt.scatter(data2['CV'], data2[column], label=column)
+
+    plt.xlabel('Index')
+    plt.ylabel('Values')
+    plt.title('STD')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f'02_RFC_7labels_figure_07', dpi=100)
+    plt.show()
+
+else:
+
+    df_temp = df_relevant.copy(deep=True)
+
+    # Splitting data into training and testing set--------------------------------------------------------------------------
+    X = df_temp.drop('quality', axis=1)
+    Y = df_temp['quality']
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20, random_state=42)
+
+    # Training the model ---------------------------------------------------------------------------------------------------
+    model = RandomForestClassifier()
+    model.fit(X_train, Y_train)
+    Y_prediction = model.predict(X_test)
+
+    # Create a confusion matrix-----------------------------------------------------------------------------------------
+    cm = confusion_matrix(Y_test, Y_prediction)
+    cm_df = pd.DataFrame(cm, index=['Actual Negative', 'Actual Positive'],
+                         columns=['Predicted Negative', 'Predicted Positive'])
+    plt.figure(figsize=(8, 6))
+    heatmap = sns.heatmap(cm_df, annot=True, fmt='d', cmap='Blues', annot_kws={"size": 18})
+    heatmap.set_xticklabels(heatmap.get_xticklabels(), fontsize=14)
+    heatmap.set_yticklabels(heatmap.get_yticklabels(), fontsize=14)
+
+    plt.title('Confusion Matrix')
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+    plt.savefig(f'02_RFC_7labels_figure_08', dpi=100)
+    plt.show()
+
+    precision = precision_score(Y_test, Y_prediction)
+    accuracy = accuracy_score(Y_test, Y_prediction)
+    recall = recall_score(Y_test, Y_prediction)
+    print("Precision:", precision)
+    print("Accuracy:", accuracy)
+    print("Recall (Sensitivity):", recall)
+
+    cv = []
+    precision1 = []
+    precision2 = []
+    accuracy1 = []
+    accuracy2 = []
+    sensitivity1 = []
+    sensitivity2 = []
+
+    for i in range(2, 21, 1):
+        scores = cross_val_score(model, X, Y, cv=i, scoring="precision", )
+        precision1.append(scores.mean())
+        precision2.append(scores.std())
+        scores = cross_val_score(model, X, Y, cv=i, scoring="accuracy")
+        accuracy1.append(scores.mean())
+        accuracy2.append(scores.std())
+        scores = cross_val_score(model, X, Y, cv=i, scoring="recall")
+        sensitivity1.append(scores.mean())
+        sensitivity2.append(scores.std())
+        cv.append(i)
+
+    data1 = pd.DataFrame(columns=['CV', 'Precision', 'Accuracy', 'Sensitivity'])
+    data1['CV'] = cv
+    data1['Precision'] = precision1
+    data1['Accuracy'] = accuracy1
+    data1['Sensitivity'] = sensitivity1
+
+    data2 = pd.DataFrame(columns=['CV', 'Precision', 'Accuracy', 'Sensitivity'])
+    data2['CV'] = cv
+    data2['Precision'] = precision2
+    data2['Accuracy'] = accuracy2
+    data2['Sensitivity'] = sensitivity2
+
+    # Plot each column as a scatter plot
+    plt.figure(figsize=(8, 6))
+    for column in ['Precision', 'Accuracy', 'Sensitivity']:
+        plt.scatter(data1['CV'], data1[column], label=column)
+
+    plt.xlabel('Index')
+    plt.ylabel('Values')
+    plt.title('MEAN')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f'02_RFC_7labels_figure_09', dpi=100)
+    plt.show()
+
+    # Plot each column as a scatter plot
+    plt.figure(figsize=(8, 6))
+    for column in ['Precision', 'Accuracy', 'Sensitivity']:
+        plt.scatter(data2['CV'], data2[column], label=column)
+
+    plt.xlabel('Index')
+    plt.ylabel('Values')
+    plt.title('STD')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f'02_RFC_7labels_figure_10', dpi=100)
+    plt.show()
